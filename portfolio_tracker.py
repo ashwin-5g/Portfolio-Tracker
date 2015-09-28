1133609050a057029d7b4a144e5c42df6a897a1b
ashwin-5g <ashwin@5gindia.net>
2015-09-28 15:58:38 +0530

    Update portfolio_tracker.py

--- portfolio_tracker.py
+++ portfolio_tracker.py
@@ -5,89 +5,104 @@ import itertools
 import re
 import sys
 
-portfolio_dict = {}
 
-def extract_symbols_and_count_from_file(filename):
+def fetch(filename):
+    """
+    This function calls the 'get_list' function which returns a set of portfolios 
+    in dict type. The dict, then, is passed onto 'compute_overallvalue' as argument
+    which returns yet another dict, with overall portfolio values as keys of this 
+    dict (and individual portfolios as values of this dict) 
+     """
 
-    port_d = {}
+    return compute_overallvalue(get_list(filename))
+    
 
-    fileobject = open(filename)
-    lines = fileobject.readlines()
-    lines = map(lambda s: s.strip(), lines)
+def get_list(filename):
+    """
+    This function extracts a list of porfolios from a file and populates an
+    empty dictionary with the extracted list.
 
+    When fully populated the dictionary would look like:
+
+    {0: [('GOOG', 50), ('MS', 10)], 1: [('GOOG', 100), ('AMZN', 90),
+    ('MS', 80)], 2: [('SGI', 100), ('GOOG', 50), ('MS', 10)]}
+    """
+    portfolios = {}
+    
+    lines = map(lambda s: s.strip(), open(filename).readlines())     
+    # opens portfolio.txt, reads the list of portfolios, removes '\n', and stores
+    # the resultant content in lines 
+    
     for i, line in enumerate(lines):
-        port_d[i] = eval(line)
 
-    for key, list_of_symbols_and_count in port_d.items():
-        port_d[key] = [((symbol.replace(' ', '').upper()), abs(count)) for symbol, count in list_of_symbols_and_count if count != 0]
+        portfolios[i] = eval(line) 
+        # eval transforms a piece of str like "[('GOOG', 50), ('MSFT', 100)]" to
+        # a list [('GOOG', 50), ('MSFT', 100)]
+        
 
-        # The above assignment ensures that all letters of symbol are capitalized, inadvertent spaces around letters of symbol removed and sequences containing 'zero' count to be rejected for further processing
+    for key, portfolio in portfolios.items():
+        portfolios[key] = [((symbol.replace(' ', '').upper()), abs(count))
+         for symbol, count in portfolio if count != 0] 
+         #fixes non-standard ways of inputting symbols and meaningless count values in 
+         #portfolio.txt
 
-    return port_d
+    return portfolios
+    
+    
+def compute_overallvalue(portfolios):
+    """
+    This function computes the overall value for each portofolio and replaces
+    the index of each portfolio with their corresponding overall value. The
+    results are returned in a dictionary form.
+    """
 
-    fileobject.close()
-
-
-def generate_value_from_symbol(symbol):
-
-    url = generate_url_from_symbol(symbol)
-    r = requests.get(url)
-    data = r.text
-    soup = BeautifulSoup(data)
-
-
-    err = str(soup.find('div', {'class': 'error'}))
-    if 'There are no results' in err:
-        print symbol, "is invalid. No corresponding stock name exists for this symbol."
-        print "Computation cannot proceed."
-        print "Program Ended"
-        sys.exit()
-
-    else:
-
-        value = str(soup.find('span', {'class': 'time_rtq_ticker'})).replace(",", "")
-        v = re.findall('[0-9]+\.[0-9]+', value)
-        v = ''.join(v)
-
-        #print "Symbol: ", symbol
-        #print "Stock Value: ", float(v)
-        return float(v)
-
-
-def generate_url_from_symbol(symbol):
-
-    url = 'http://in.finance.yahoo.com/q?s=' + symbol
-    return url
-
-
-def compute_overall_value_for_each_portfolio(port_dict):
-
-    for k in port_dict.keys():
+    for k in portfolios.keys():
         overall_value = 0
 
-        for symbol, count in port_dict[k]:
-            overall_value = overall_value + (generate_value_from_symbol(symbol) * count)
+        for symbol, count in portfolios[k]:
+            overall_value = overall_value + (get_value(symbol) * count)
 
-        port_dict[overall_value] = port_dict[k]
-        del port_dict[k]
+        portfolios[overall_value] = portfolios[k]
+        del portfolios[k]
+    return portfolios
 
-    #print port_dict
 
-
-def display_portfolios_in_descending_order_of_overall_value(port_dict):
-
-    for k in sorted(port_dict, reverse=True):
-        for x, y in port_dict[k]:
+def sort_descending_order(portfolios):
+    """
+    This function takes in a dict, sorts it based on the indices and prints
+    out the list of portfolios in the manner described in the problem
+    specification.
+    """
+    for k in sorted(portfolios, reverse=True):
+        for x, y in portfolios[k]:
             print "{0} - {1} ".format(x.replace(' ', ''), y),
         print
+
+
+def get_value(symbol):
+    """
+    This function will fetch the current stock value from the
+    Yahoo server when it receives a valid symbol name.
+    If the symbol is invalid, the program halts.
+    """
+    url = 'http://in.finance.yahoo.com/q?s=' + symbol
+    r = requests.get(url)
+    data = r.text
+    soup = BeautifulSoup(data, "lxml") 
+    # fetches html code that corresponds to 'url'
+    #and stores that in 'soup'                      
+    print soup
+
+    value = str(soup.find('span', {'class': 'time_rtq_ticker'})).replace(",", "")
+    v = re.findall('[0-9]+\.[0-9]+', value)
+    v = ''.join(v)
+    return float(v) 
+    # above four statements help extract symbol value using 
+    # 'soup', 'findall', and str-manipulation functions like 'replace' and 'join'
 
 
 if __name__ == '__main__':
 
     script, filename = argv
-
-    portfolio_dict = extract_symbols_and_count_from_file(filename)
-
-    compute_overall_value_for_each_portfolio(portfolio_dict)
-
-    display_portfolios_in_descending_order_of_overall_value(portfolio_dict)
+    portfolios = fetch(filename)
+    sort_descending_order(portfolios)
